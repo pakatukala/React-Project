a49744c29d606cd8fe467d89624ca77106967328
pavankatukala1990 <pavan.katukala@gmail.com>
2019-02-28 21:40:06 -0600

    first commit

---
+++ starter/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//const element = React.createElement('div', null, 'React element!');
+
+const element = <div>React Element</div>
+
+ReactDOM.render(element, document.getElementById('root'));
